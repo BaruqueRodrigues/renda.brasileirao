9bbcf6be9de09be78566d2d82416ad050686a8d2
Baruque Rodrigues <81967584+BaruqueRodrigues@users.noreply.github.com>
2022-07-13 12:24:54 -0300

    baixar varios campeonatos

--- R/baixa_rendas_brasileirao_b.R
+++ R/baixa_rendas_brasileirao_b.R
@@ -1,15 +1,27 @@
 #' baixa_rendas_brasileirao_b
 #'
 #' @param n_rodadas vetor com número de rodadas
+#' @param ano ano do campeonato
 #'
 #' @return dataframe com os dados sobre o financiamento das partidas da série B do brasileirão
 #' @export
 #' @import httr
 #' @examples
-#' baixa_rendas_brasileirao_b(1:15)
-baixa_rendas_brasileirao_b <- function(n_rodadas){
+#' baixa_rendas_brasileirao_b(1:15, 2015)
+baixa_rendas_brasileirao_b <- function(n_rodadas, ano){
   
   executa_download <- function(n_rodadas){
+    
+    dados_ex <- dplyr::tibble(ano_camp = seq(2012, 2022, 1),
+                              id_fase = c(2, 404, 665, 1032, 1375,
+                                          1639, 1870, 2321, 2585, 2805,
+                                          2805))
+    
+    
+    id_fase <- dados_ex %>% 
+      dplyr::filter(ano_camp == ano) %>%
+      dplyr::pull(id_fase)
+    
     
     rodadas <- n_rodadas
     
@@ -17,7 +29,9 @@ baixa_rendas_brasileirao_b <- function(n_rodadas){
     
     queryString <- list('ajax' = "get_ranking2")
     
-    payload <- paste0("id_fase=3110&rodada=",
+    payload <- paste0("id_fase=",
+                      id_fase,
+                      "&rodada=",
                       rodadas,
                       "&=&sort=")
     
@@ -32,7 +46,7 @@ baixa_rendas_brasileirao_b <- function(n_rodadas){
           authority = 'www.srgoool.com.br',
           accept_language = 'pt-BR,pt;q=0.9',
           origin = 'https://www.srgoool.com.br',
-          referer = 'https://www.srgoool.com.br/classificacao/Brasileirao/Serie-B/2022',
+          referer = stringr::str_glue('https://www.srgoool.com.br/classificacao/Brasileirao/Serie-B/{ano}'),
           user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'),
         query = queryString,
         content_type("application/x-www-form-urlencoded"),
@@ -50,7 +64,15 @@ baixa_rendas_brasileirao_b <- function(n_rodadas){
   
   dados <- n_rodadas %>% 
     purrr::map_dfr(executa_download, .id = "rodadas") %>% 
-    tibble::tibble()
+    tibble::tibble() %>% 
+    dplyr::mutate(pagante = as.numeric(pagante),
+                  ingresso = as.numeric(ingresso),
+                  ingresso_vendido = as.numeric(ingresso_vendido),
+                  renda_bruta = as.numeric(renda_bruta),
+                  renda_liq_sort = as.numeric(renda_liq_sort),
+                  renda_liq = as.numeric(renda_liq),
+                  valor_renda_liq = as.numeric(valor_renda_liq),
+                  data = paste0(data, "/", ano, sep = "") %>% lubridate::dmy())
   
   dados 
 }
